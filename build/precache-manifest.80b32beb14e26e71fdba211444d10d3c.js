aaf5d4f64928c71c9ae84e442840898a79c55b2a
Zak Sakata <zakattack26111@gmail.com>
2018-10-24 20:07:01 -0700

    adding initial files

---
+++ build/precache-manifest.80b32beb14e26e71fdba211444d10d3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e32ac2dd1b42ef41baa",
+    "url": "/static/js/main.2e32ac2d.chunk.js"
+  },
+  {
+    "revision": "96f0cc38d689314eb0a2",
+    "url": "/static/js/1.96f0cc38.chunk.js"
+  },
+  {
+    "revision": "2e32ac2dd1b42ef41baa",
+    "url": "/static/css/main.2f52d9fa.chunk.css"
+  },
+  {
+    "revision": "f69ab8b0cb50e901e7e025ec703d81e4",
+    "url": "/index.html"
+  }
+];
