4b9839fbca115310a1b6ee19bb654db3a5d0ca66
ageronimo <angelageroniimo@gmail.com>
2018-10-30 06:55:42 -1000

    started message interface

---
+++ src/inbox.js
@@ -0,0 +1,14 @@
+console.log('hello');
+
+/* needed functions:
+ *  active notifications/messages
+ *  + only on messages
+ *  new message
+ *   
+ *  indicate unread message
+ * 
+*/
+
+function goToMsg() {
+
+}
