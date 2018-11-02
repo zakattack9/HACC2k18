c271cf814126319cb4bdcc97ac7e67491f5a3cd8
ageronimo <angelageroniimo@gmail.com>
2018-11-02 12:17:32 -1000

    polishing + added create message functionality

--- src/inbox.js
+++ src/inbox.js
@@ -4,7 +4,8 @@ console.log('hello');
  *  + only visible on messages
  *  new message - waiting for search functionality
  *  indicate unread message
- * dynamically change #convoContainer to bring up conversations between users
+ *  dynamically change #convoContainer to bring up conversations between users
+ *  (not a func) move functions from html to js
 */
 
 /* GENERAL NAVIGATION */
@@ -14,66 +15,89 @@ $(document).ready(() => {
 });
 
 const activateNotifTab = () => {
-    $("#notificationContainer").removeClass("inactive");
-    $("#notificationContainer").addClass("block-display");
     $("#notificationsButton").addClass("tabButtonStyle");
-    $("#messagePreviewContainer").removeClass("grid-display");
     $("#messagesButton").removeClass("tabButtonStyle");
-    $("#messagePreviewContainer").addClass("inactive")
+    $("#notificationContainer").addClass("block-display");
+    $("#notificationContainer").removeClass("inactive");
+    $("#messagePreviewContainer").removeClass("grid-display");
+    $("#messagePreviewContainer").addClass("inactive");
+    $("#newMsgButton").removeClass("block-display");
+    $("#newMsgButton").addClass("inactive");
     $("#convoContainer").removeClass("grid-display");
     $("#convoContainer").addClass("inactive");
+    $("#newMsgContainer").removeClass("grid-display");
+    $("#newMsgContainer").addClass("inactive");
     // $("#notificationContainer").show();
     // $("#messagePreviewContainer").hide();
     // $("#convoConatiner").hide();
 }
 
 const activateMsgTab = () => {
-    $("#notificationContainer").removeClass("block-display");
     $("#notificationsButton").removeClass("tabButtonStyle");
-    $("#notificationContainer").addClass("inactive");
-    $("#messagePreviewContainer").removeClass("inactive")
-    $("#messagePreviewContainer").addClass("block-display");
     $("#messagesButton").addClass("tabButtonStyle");
+    $("#notificationContainer").removeClass("block-display");
+    $("#notificationContainer").addClass("inactive");
+    $("#messagePreviewContainer").removeClass("inactive");
+    $("#messagePreviewContainer").addClass("block-display");
+    $("#newMsgButton").addClass("block-display");
+    $("#newMsgButton").removeClass("inactive");
     $("#convoContainer").removeClass("grid-display");
     $("#convoContainer").addClass("inactive");
+    $("#newMsgContainer").removeClass("grid-display");
+    $("#newMsgContainer").addClass("inactive");
     // $("#notificationContainer").hide();
     // $("#messagePreviewContainer").show();
     // $("#convoConatiner").hide();
 }
 
 const activateConvo = () => {
+    //need function to check identifier and push up relevant conversation
     $("#notificationContainer").removeClass("block-display");
     $("#notificationContainer").addClass("inactive");
-    $("#messagePreviewContainer").removeClass("block-display")
+    $("#messagePreviewContainer").removeClass("block-display");
     $("#messagePreviewContainer").addClass("inactive");
     $("#convoContainer").removeClass("inactive");
     $("#convoContainer").addClass("grid-display");
+    $("#newMsgContainer").removeClass("grid-display");
+    $("#newMsgContainer").addClass("inactive");
     scrollToBottom();
 }
 
-$("#notificationsButton").click(activateNotifTab);
-$("#messagesButton").click(function () {
-    activateMsgTab();
-});
+const activateNewMsg = () => {
+    $("#notificationsButton").removeClass("tabButtonStyle");
+    $("#messagesButton").addClass("tabButtonStyle");
+    $("#notificationContainer").removeClass("block-display");
+    $("#notificationContainer").addClass("inactive");
+    $("#messagePreviewContainer").removeClass("inactive");
+    $("#messagePreviewContainer").addClass("block-display");
+    $("#newMsgButton").removeClass("block-display");
+    $("#newMsgButton").addClass("inactive");
+    $("#convoContainer").removeClass("grid-display");
+    $("#convoContainer").addClass("inactive");
+    $("#newMsgContainer").addClass("grid-display");
+    $("#newMsgContainer").removeClass("inactive");
+}
 
-
+//button clicking
+$(document).ready(() => {
+    $("#notificationsButton").on('click', () => {activateNotifTab();});
+    $("#messagesButton").on('click', () => {activateMsgTab();});
+    $(".msgBackButton").on('click', () => {activateMsgTab();});
+    //navigates user to Messages tab after clicking "received message" notification
+    $(".gotoMsg").on('click', () => {activateMsgTab();}); 
+    //open conversation
+    $(".messagePreviewItem").on('click', () => {activateConvo();});
+    $("#sendButton").on('click', () => {sendTypedMsg();});
+    $("#newMsgButton").on('click', () => {activateNewMsg()});
+})
 
 //working with #notificationContainer, #notificationsButton,
 //  #messagePreviewContainer, #messagesButton
 //if $().css('property) == 'value' => do something
 
 /* NOTIFICATION functionality */
-//navigates user to Messages tab after clicking "received message" notification
-$(document).ready(() => {
-    $(".gotoMsg").click(() => {
-        activateMsgTab();
-    }); 
-});
-
 
 /* MESSAGING functionality */
-//open conversation
-
 
 //scrolls user to bottom of conversation when opened
 const scrollToBottom = () => {
@@ -127,6 +151,23 @@ const reMsgPreview = () => {
     console.log("last msg: " + lastMsg);
 }
 
-// $(document).ready(() => {
-//     $(document).on("DOMSubTreeModified", ".user-id-4-convo-user-id-2", reMsgPreview());
-// })
+
+/* NEW MESSAGE FUNCTIONALITY */
+const searchFriends = () => {
+    var input, filter, friendInstance, searchName;
+    input = $("#friendSearchInput");
+    filter = input.val().toUpperCase();
+    friendInstance = $(".friendItem");
+    for (var i = 0; i < friendInstance.length; i++) {
+        searchName = friendInstance[i].getElementsByClassName("friendName")[0];
+      if (searchName.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        friendInstance[i].style.display = "";
+      } else {
+        friendInstance[i].style.display = "none";
+      }
+    }
+}
+
+$(document).ready(() => {
+    $("#friendSearchInput").keyup(searchFriends());
+})
