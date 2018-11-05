75e4622b4993693b8bc3c377bca339f193555f09
ageronimo <angelageroniimo@gmail.com>
2018-11-05 06:56:29 -1000

    major features + changed file location to public

--- public/js/inbox.js
+++ public/js/inbox.js
@@ -19,7 +19,7 @@ const activateNotifTab = () => {
     $("#messagesButton").removeClass("tabButtonStyle");
     $("#notificationContainer").addClass("block-display");
     $("#notificationContainer").removeClass("inactive");
-    $("#messagePreviewContainer").removeClass("grid-display");
+    $("#messagePreviewContainer").removeClass("block-display");
     $("#messagePreviewContainer").addClass("inactive");
     $("#newMsgButton").removeClass("block-display");
     $("#newMsgButton").addClass("inactive");
@@ -102,10 +102,12 @@ $(document).ready(() => {
 //scrolls user to bottom of conversation when opened
 const scrollToBottom = () => {
     var objDiv = $('#messageContainer');
-    objDiv.scrollTop(objDiv[0].scrollHeight);
+    if (objDiv.length > 0) {
+        objDiv.scrollTop(objDiv[0].scrollHeight);
+    }
 }
 
-window.onload = scrollToBottom;
+// window.onload = scrollToBottom;
 
 //get date and time
 const currDateTime = () => {
@@ -125,12 +127,40 @@ const currDateTime = () => {
 const sendTypedMsg = () => {
     let text = $("#msgInput").val();
     if ($("#msgInput").val().length > 1) {
+        //creates new msg on FE
         $("#messageContainer").append(`
             <div class="messageItem native-user">
                 <p>${text}</p>
                 <span class="date-and-time">${currDateTime()}</span>
             </div>
         `);
+
+        //creates new msg in storage
+        // loop through storage and find matching user, then push to messages
+        for (var i = 0; i < storage.length; i++) {
+            if (Object.values(storage[i]).indexOf($("#userBar").attr('class')) > -1) {
+                var newMsg = {
+                    sender: `${nativeUserId}`,
+                    timeSent: `${currDateTime()}`,
+                    msgContent: `${text}`
+                }
+                console.log(newMsg);
+                storage[i].messages.push(newMsg);
+                
+                //creates msg preview item
+                if ($(".messagePreviewItem").hasClass(`${storage[i].convoWith}`) == false) {
+                    console.log('creating new preview');
+                    addMsgPreview(storage[i], storage[i].messages[storage[i].messages.length - 1]);
+                } else {
+                    $(`.${storage[i].convoWith} .msgContent .time-ago`).empty();
+                    $(`.${storage[i].convoWith} .msgContent .time-ago`).html(`
+                        <span class="time-ago">1s ago</span>
+                    `);
+                    console.log('try again');
+                }
+            }
+        }
+        update();
     }
     scrollToBottom();
     $("#msgInput").val('');
@@ -142,15 +172,6 @@ $(document).keypress((key) => {
         sendTypedMsg();
     }
 })
-
-//return to Messages tab from Convo
-
-//show most recent message in message preview WIP
-const reMsgPreview = () => {
-    let lastMsg = $(".messageItem p").last().text();
-    console.log("last msg: " + lastMsg);
-}
-
 
 /* NEW MESSAGE FUNCTIONALITY */
 const searchFriends = () => {
@@ -171,3 +192,150 @@ const searchFriends = () => {
 $(document).ready(() => {
     $("#friendSearchInput").keyup(searchFriends());
 })
+
+
+/* MESSAGE GENERATION */
+$(document).ready(() => {
+    $(".messagePreviewItem").on('click', function() {
+        generateConvo(this);
+    });
+    $(".friendItem").on('click', function() {
+        generateConvo(this);
+        activateConvo();
+    });
+})
+
+function generateConvo(el) {
+    var userId = $(el).attr('class').split(" ")[1];
+    console.log("User queried: " + userId);
+
+    //finds index of object in storage and returns true if found
+    var idIndex;
+    function checkObj(id) {
+        for (var i = 0; i < storage.length; i++) {
+            console.log("data found: ", storage[i]);
+            if (Object.values(storage[i]).indexOf(id) > -1) {
+                idIndex = i;
+                return true;
+            } else if (Object == undefined) {
+                return false;
+            }
+        }
+    }
+
+    //goes to build function
+    var username = $(el).find(".user-name").html();
+    var iconSrc = $(`.${userId} .user-icon img`).attr('src');
+    if (checkObj(userId) == true) {
+        buildConvo(storage[idIndex]);
+    } else {
+        var obj = {
+            convoWith: `${userId}`,
+            username: `${username}`,
+            userIcon: `${iconSrc}`,
+            messages: []
+        }
+        storage.push(obj);
+        console.log(storage);
+        buildConvo(obj);
+    }
+}
+
+function buildMsgs(obj) {
+    if (obj.messages.length > 0) {
+        for (var i = 0; i < obj.messages.length; i++) {
+            console.log("inserting message " + i);
+            //reusable object var
+            var msgObj = obj.messages[i];
+            
+            //determine user type
+            var userType;
+            function determineUser() {
+                if (msgObj.sender == nativeUserId) {
+                    userType = "native-user";
+                } else {
+                    userType = "non-native-user";
+                }
+            }
+            determineUser();
+
+            //build convo
+            console.log(msgObj);
+            $("#messageContainer").append(`
+                <div class="messageItem ${userType}">
+                    <p>${msgObj.msgContent}</p>
+                    <span class="date-and-time">${msgObj.timeSent}</span>
+                </div>
+            `);
+        }
+        scrollToBottom();
+    }
+}
+
+function buildConvo(build) {
+    $("#convoContainer").empty();
+    $("#convoContainer").append(`
+        <div id="userBar" class="${build.convoWith}">
+            <div class="msgBackButton" onclick="activateMsgTab()">
+                <i class="fas fa-angle-left"></i> 
+            </div>
+            <div class="user-name ${build.convoWith}">${build.username}</div>
+            <div class="user-icon">
+                <img src="${build.userIcon}">
+            </div>
+        </div>
+        <div id="conversation" class="${nativeUserId}-msg-${build.convoWith}">
+            <div id="messageContainer">
+            </div>
+        </div>
+        <div id="sendContainer">
+            <input type="text" id="msgInput" placeholder="Send a message">
+            <div id="sendButton" onclick="sendTypedMsg()">
+            <i class="fas fa-paper-plane"></i>
+            </div>
+        </div>
+    `);
+    buildMsgs(build);
+}
+
+$(document).ready(() => {
+    update();
+})
+
+function update() {
+    $(".messagePreviewItem").each(function () {
+        updateMsgPreview(this);
+    });
+}
+
+function updateMsgPreview(el) {
+    console.log(el);
+    var userId = $(el).attr('class').split(" ")[1];
+    console.log("id: ", userId);
+    for (var i = 0; i < storage.length; i++) {
+        if (Object.values(storage[i]).indexOf(userId) > -1) {
+            var obj = storage[i];
+            var lastMsg = obj.messages[obj.messages.length - 1].msgContent;
+            console.log(lastMsg);
+            $(`.${userId} .msgContent p`).empty();
+            $(`.${userId} .msgContent p`).html(`
+                <b class="user-name">${obj.username}</b>: ${lastMsg}
+            `);
+        }
+    }
+}
+
+function addMsgPreview(obj, lastMsg) {
+    $("#messagePreviewContainer").prepend(`
+    <div class="messagePreviewItem ${obj.convoWith}" onclick="generateConvo(this) activateConvo()">
+        <div class="user-icon">
+            <img src="${obj.userIcon}">
+        </div>
+        <div class="msgContent">
+            <p><b class="user-name">${obj.username}</b>: ${lastMsg}</p>
+            <span class="time-ago">1s ago</span>
+        </div>
+    </div>
+    `);
+}
+
